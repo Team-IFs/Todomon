697f99abb174a6d92d1c2f0d7fcfcd0f5bf630ce
yedin123 <aas9919@naver.com>
2023-06-07 03:38:35 +0900

    :memo: Docs: add Authorization

--- src/main/java/com/ifs/back/config/SwaggerConfiguration.java
+++ src/main/java/com/ifs/back/config/SwaggerConfiguration.java
@@ -1,13 +1,14 @@
 package com.ifs.back.config;
 
 import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -18,6 +19,8 @@ public class SwaggerConfiguration {
   @Bean
   public Docket apiV1() {
     return new Docket(DocumentationType.SWAGGER_2)
+        .securityContexts(List.of(this.securityContext())) // SecurityContext 설정
+        .securitySchemes(List.of(this.apiKey())) // ApiKey 설정
         .groupName("Member")
         .select()
         .apis(RequestHandlerSelectors.
@@ -31,6 +34,8 @@ public class SwaggerConfiguration {
   public Docket apiV2() {
     return new Docket(DocumentationType.SWAGGER_2)
         .useDefaultResponseMessages(false)
+        .securityContexts(List.of(this.securityContext()))
+        .securitySchemes(List.of(this.apiKey()))
         .groupName("Todo")
         .select()
         .apis(RequestHandlerSelectors.
@@ -44,6 +49,8 @@ public class SwaggerConfiguration {
   public Docket apiV3() {
     return new Docket(DocumentationType.SWAGGER_2)
         .useDefaultResponseMessages(false)
+        .securityContexts(List.of(this.securityContext()))
+        .securitySchemes(List.of(this.apiKey()))
         .groupName("Category")
         .select()
         .apis(RequestHandlerSelectors.
@@ -57,6 +64,8 @@ public class SwaggerConfiguration {
   public Docket apiV4() {
     return new Docket(DocumentationType.SWAGGER_2)
         .useDefaultResponseMessages(false)
+        .securityContexts(List.of(this.securityContext()))
+        .securitySchemes(List.of(this.apiKey()))
         .groupName("Friend")
         .select()
         .apis(RequestHandlerSelectors.
@@ -70,6 +79,8 @@ public class SwaggerConfiguration {
   public Docket apiV5() {
     return new Docket(DocumentationType.SWAGGER_2)
         .useDefaultResponseMessages(false)
+        .securityContexts(List.of(this.securityContext()))
+        .securitySchemes(List.of(this.apiKey()))
         .groupName("Follow")
         .select()
         .apis(RequestHandlerSelectors.
@@ -90,5 +101,24 @@ public class SwaggerConfiguration {
         "https://naver.com",
         new ArrayList<>()
     );
+  }
+
+  // JWT SecurityContext 구성
+  private SecurityContext securityContext() {
+    return SecurityContext.builder()
+        .securityReferences(defaultAuth())
+        .build();
+  }
+
+  private List<SecurityReference> defaultAuth() {
+    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+    return List.of(new SecurityReference("Authorization", authorizationScopes));
+  }
+
+  // ApiKey 정의
+  private ApiKey apiKey() {
+    return new ApiKey("Authorization", "Authorization", "header");
   }
 }
