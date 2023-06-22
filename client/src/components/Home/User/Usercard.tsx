f39367dcdc3190636a17c3001e5ac7de91f1043b
MinaRoh <00minha@gmail.com>
2023-06-22 12:10:05 +0900

    ✨ Feat: add auto login

--- client/src/components/Home/User/Usercard.tsx
+++ client/src/components/Home/User/Usercard.tsx
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled';
+import { useState } from 'react';
 import { useRecoilState } from 'recoil';
-import { IsLogin, UserInfo } from '../../../recoil/atoms/atoms';
+import { UserInfo } from '../../../recoil/atoms/atoms';
+import { getCookie } from '../../../utils/cookies/cookies';
+import { getCurrentUserInfo } from '../../../utils/userInfo';
 
 const Card = styled.div({
   display: 'flex',
@@ -31,8 +34,19 @@ const InfoCard = styled.div({
 
 
 const UserCard = () => {
-  const [isLogin] = useRecoilState(IsLogin);
-  const [userInfo] = useRecoilState(UserInfo);
+  const [userInfo, setUserInfo] = useRecoilState(UserInfo);
+  const [userInfoLoaded, setUserInfoLoaded] = useState(false);
+
+   // 토큰이 있으면 로그인 상태 유지하기
+  if (!userInfoLoaded) {
+    if (getCookie('accessJwtToken') && getCookie('refreshJwtToken')) {
+      getCurrentUserInfo().then(userInfo => {
+          if(userInfo) setUserInfo(userInfo);
+      });
+      setUserInfoLoaded(true);
+    }
+  }
+
 
   return (
     <Card>
@@ -58,8 +72,8 @@ const UserCard = () => {
         </svg>
       </UserCat>
       <InfoCard>
-        <div className='name'>{isLogin ? `${userInfo.nickname}` : '비로그인유저'}</div>
-        <div>{isLogin && userInfo.bio ? `${userInfo.bio}` : ''}</div>
+        <div className='name'>{userInfo? `${userInfo.nickname}` : '비로그인유저'}</div>
+        <div>{userInfo && userInfo.bio ? `${userInfo.bio}` : ''}</div>
       </InfoCard>
     </Card>
   )
