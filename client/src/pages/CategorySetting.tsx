c0d15d81a29d8c5b5870534c83a57e9f0f080718
MinaRoh <00minha@gmail.com>
2023-06-25 12:35:13 +0900

    ✨ Feat: protect auth routers

--- client/src/pages/CategorySetting.tsx
+++ client/src/pages/CategorySetting.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRouter } from '../hooks/useRouter';
+import { useRecoilState } from 'recoil';
+import { IsLogin } from '../recoil/atoms/atoms';
 
 const CategorySetting = () => {
+  const { routeTo } = useRouter();
+  const [isLogin] = useRecoilState(IsLogin);
 
+  useEffect(() => {
+    if (!isLogin) {
+        alert('로그인이 필요한 페이지입니다.')
+        routeTo('/login')
+      }
+  });
   return (<div>
     <h1>
       CategorySetting
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,9 +2,23 @@ import styled from '@emotion/styled';
 import UserCard from '../components/Home/User/Usercard';
 import TodoContainer from '../components/Home/Todo/TodoContainer';
 import Calendar from '../components/Home/Calendar/Calendar';
-import React from 'react';
+import React, {useEffect} from 'react';
+import { IsLogin } from '../recoil/atoms/atoms';
+import { useRouter } from '../hooks/useRouter';
+import { useRecoilState } from 'recoil';
 
 const Home = () => {
+  const { routeTo } = useRouter();
+  const [isLogin] = useRecoilState(IsLogin);
+
+  useEffect(() => {
+    if (!isLogin) {
+        alert('로그인이 필요한 페이지입니다.')
+        routeTo('/login')
+      }
+  });
+  
+
   const HomePage = styled.div({
     display: 'flex',
     flexDirection: 'row',
