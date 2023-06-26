07bf67fefa8b8d0b92374ce157ac8bb9ce608d08
MinaRoh <00minha@gmail.com>
2023-06-26 17:22:47 +0900

    💄 Style: add personal info ui

--- client/src/components/Settings/AccountSetting.tsx
+++ client/src/components/Settings/AccountSetting.tsx
@@ -1,8 +1,69 @@
+import styled from '@emotion/styled';
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
+import { useRecoilState } from 'recoil';
+import { UserInfo } from '../../recoil/atoms/atoms';
+
+  const TitleContainer = styled.div({
+    display: 'flex',
+    flexDirection: 'column',
+    margin: '20px'
+  })
+  const ContentContainer = styled.div({
+    display: 'flex',
+    flexDirection: 'column',
+    width: '300px',
+    gap: '20px',
+    margin: '0px 20px',
+  })
+  const InputContainer = styled.div({
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    fontWeight: 'bold',
+    gap: '20px',
+    margin: '0px 20px'
+  })
+  const ButtonContainer = styled.div({
+    display: 'flex',
+    justifyContent: 'end',
+    width: '200px',
+  })
+
 const AccountSetting = () => {
+  const [userInfo, setUserInfo] = useRecoilState(UserInfo);
+
   return (
-    <div>
-      개인정보관리
-    </div>
+    <TitleContainer>
+      <h2>개인정보관리</h2>
+      <ContentContainer>
+        <InputContainer>
+          <label>닉네임</label>
+          <ButtonContainer>
+            <Input type='text' fullWidth={true} defaultValue={userInfo.nickname} />
+          </ButtonContainer>
+        </InputContainer>
+        <InputContainer>
+          <label>자기소개</label>
+          <ButtonContainer>
+            <Input type='text' fullWidth={true} defaultValue={userInfo.bio} />
+          </ButtonContainer>
+        </InputContainer>
+        <InputContainer>
+          <label>비밀번호</label>
+          <ButtonContainer>
+            <Button type='submit' variant='outlined' fullWidth={true}>비밀번호 변경</Button>
+          </ButtonContainer>
+        </InputContainer>
+        <InputContainer>
+          <label>계정삭제</label>
+          <ButtonContainer>
+            <Button type='submit' variant='outlined' fullWidth={true}>회원가입</Button>
+          </ButtonContainer>
+        </InputContainer>
+      </ContentContainer>
+    </TitleContainer>
   )
 };
 export default AccountSetting;
